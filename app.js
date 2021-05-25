e202b682eb3d0e99c17ab2ad46930c9afe02811b
Ahmet Aydın <ahmet@hakanaydin.me>
2021-05-25 15:26:28 +0300

    ejs module has been added and necessary redirects have been made

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const app = express();
 
+//Template Engine
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    const blog = {
-        id:1,
-        title: "Blog Title",
-        description: "Blog description"
-    }
-    res.send(blog)
-})
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Sunucu ${port} portunda başlatıldı..`);
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
 });
