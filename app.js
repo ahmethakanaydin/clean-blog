610532e4218cbe0df9391fbd6b10fcdada32eb28
Ahmet Aydın <ahmet@hakanaydin.me>
2021-05-27 18:14:19 +0300

    Individual Post Pages

--- app.js
+++ app.js
@@ -3,28 +3,25 @@ const mongoose = require('mongoose');
 const Post = require('./models/Post');
 const app = express();
 
-
 //connect DB
 mongoose.connect('mongodb://localhost/cleanblog-test-db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-
 //Template Engine
 app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 //Routes
 app.get('/', async (req, res) => {
-  const posts = await Post.find({})
+  const posts = await Post.find({});
   res.render('index', {
-    posts
+    posts,
   });
 });
 app.get('/about', (req, res) => {
@@ -37,10 +34,17 @@ app.get('/post', (req, res) => {
   res.render('post');
 });
 
+app.get('/posts/:id', async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render('post', {
+    post,
+  });
+});
+
 app.post('/posts', async (req, res) => {
-  await Post.create(req.body)
+  await Post.create(req.body);
   res.redirect('/');
-})
+});
 
 const port = 3000;
 app.listen(port, () => {
